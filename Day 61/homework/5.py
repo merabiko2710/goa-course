16fb9f9169a042ef20473bda539a2c1df21a95d2
merabiko2710 <mindia.gaganidze@yahoo.com>
2024-09-03 15:49:39 +0400

    commit

--- Day 61/homework/5.py
+++
@@ -1,8 +0,0 @@
-# 5) მომხმარებელს შემოატანინეთ რიცხვი შემდეგ შექმენით ფუნქცია რომელიც მიიღებს პარამტეტრად მომხმარებლის შემოტანილ რიცხვს ფუნქციამ უნდა დააბრუნოს ეს რიცხვი არის თუ არა ხუთის ჯერადი
-number = int(input("number1"))
-def five(intsa):
-    if intsa % 5 == 0:
-        print("intsa GOAelia")
-    else:
-        print("intsa novatoristia")
-five(number)
