22dcf417e17025829d41658564bcd9eab44bfb8e
merabiko2710 <mindia.gaganidze@yahoo.com>
2024-09-03 15:48:09 +0400

    commit

---
+++ Day 61/classwork/.py
@@ -0,0 +1,5 @@
+pop = int(input("num num num "))
+popa = int(input("num num num "))
+def popy (num1, num2):
+    print(num1 + num2)
+popy(pop ,popa)
---
+++ Day 61/homework/hw8.py
@@ -0,0 +1,6 @@
+# 8) შექმენი ფუნქცია, რომელიც შემოატანინებს მომხარებელს ორ რიცხვს და დააბრუნებს მათ ჯამს დამატებული 5.
+def g():
+    num=int(input("enter num: "))
+    numy=int(input("enter nother num: "))
+    print(num + numy + 5)
+g()
