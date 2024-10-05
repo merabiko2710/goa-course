1f7a878642bc66d0c35201b59d6f545b0f0cc917
merabiko2710 <mindia.gaganidze@yahoo.com>
2024-10-05 17:30:13 +0400

    commit

---
+++ Day 72/.py
@@ -0,0 +1,6 @@
+ #  1) შექმენით ფუნქცია რომელიც მიიღებს ადამიანის სახელს და შემდეგ დაუპრინტავს მისასალმებელ წერილს
+int = input('name: ')
+def hello_name(inta):
+    return "hello " + inta
+print(hello_name(int))
+    
