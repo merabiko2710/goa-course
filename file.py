44f0d4b747c8ad104d2c18f3b2043ba8f99cfb41
merabiko2710 <merabikogaganidze@gmail.com>
2025-03-01 18:47:46 +0400

    commit

--- file.py
+++ file.py
@@ -1,3 +1,21 @@
 #find the terrorist in tbillisi
-guga_qwertydze = (6*3+(12+34))+2/3*3
-lila_lalidze = (354/2)
+guga_qwertydze = 20
+lila_lalidze = 67 
+jeffery_dahmer = 66.200
+ilia_tophuria = 12
+nlack_bigga = 49
+
+if guga_qwertydze > lila_lalidze:
+    answer="maybe guga_qwertydze"
+    print (answer)
+else:
+    answer="maybe lila_lalidze"
+    print (answer)
+
+if answer > str ( jeffery_dahmer ):
+    answer2 = "maybe jeffery "
+    print (answer2)
+else:
+    answer2 = answer
+    print (answer2)
+
