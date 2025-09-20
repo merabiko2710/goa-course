d34f2d35ee9112138b0cb437cef7d768aa20ed24
merabiko2710 <merabikogaganidze@gmail.com>
2025-09-20 12:04:27 +0400

    commit

--- Day 167/homework/script.js
+++ Day 167/homework/script.js
@@ -4,7 +4,7 @@
 
 // მაგ: "hello" → 5.
 function name1(nam1) {
-    console.log(nam1.length())
+    console.log(nam1.length)
    
 }
 name1(prompt("give me a name: "))
@@ -14,11 +14,15 @@ name1(prompt("give me a name: "))
 
 // მაგ: 8 → 0, 2, 4, 6, 8.
 function name2(name1) {
-    for (let i = 0;name1 >i; i+=2){
+    for (let i = 0;i <= name1; i++){
+        if (i % 2 ===0){
+            console.log(i)
+        }
 
     }
+
 }
-name2()
+name2(45)
 // 3.
 
 // შექმენი ფუნქცია, რომელსაც გადაეცემა რიცხვი.
@@ -36,14 +40,14 @@ function name3(num4){
     }
 }
 
-name3()
+name3(prompt("GIVE ME MY NUMBER ! ! ! "))
 // 4.
 
 // შექმენი ფუნქცია, რომელიც არგუმენტად მიიღებს ორ რიცხვს და დააბრუნებს მათ ჯამს.
 
 // მაგ: (5, 7) → 12.
 function HelloNostalgia5(name, eman){
-    console.log(name * eman)
+    console.log(name + eman)
 }
 // 5.
 
@@ -54,7 +58,8 @@ function name10(name11) {
     for (let i = 1; name11.length > i; i++) {
     console.log(name11[i])
 }
-return result
+    result+= name11
+    return result
 }
 console.log(name10('Hello'))
 // მაგ: "Giorgi" → "iorgi".
